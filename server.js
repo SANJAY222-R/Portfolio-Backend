9a6cdc7a57c430a8f0acd6236cd3894ce843af5b
SANJAY222-2006 <sanjay236002@gmail.com>
2025-08-26 21:26:05 +0530

    final authhh

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import User from "./models/authModel.js";
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  // origin: 'http://localhost:5173',
   credentials: true 
 }));
 app.use(express.json());
